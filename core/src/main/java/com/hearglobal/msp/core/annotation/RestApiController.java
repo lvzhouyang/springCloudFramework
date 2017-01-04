9b0c26f934280cab41e7c4e7a4c523c2c4141550
chencg <chencg@hearglobal.com>
2017-01-04 14:36:49 +0800

    update annotation doc

--- core/src/main/java/com/hearglobal/msp/core/annotation/RestApiController.java
+++ core/src/main/java/com/hearglobal/msp/core/annotation/RestApiController.java
@@ -9,33 +9,27 @@ import java.lang.annotation.*;
 
 /**
  * Created by ccg on 2017/1/4.
+ *
+ * 该注解封装了 RestController 和 RequestMapping
+ *
+ * 添加默认配置前缀 "/api" 用于远程的API调用
+ *
+ * 配置可更改在@RestApiController(自定义)
+ *
  */
-
-//源注解 声明注解范围  要不写就不认识自定义的注解了
-//声明的 type  可用于 Class, interface (including annotation type), or enum declaration
-@Target({ElementType.TYPE})
-//保留    保留策略：运行时保留  这类注解会保留在三个阶段 source 源代码  class 编译文件  runtime 运行时
+@Target({ ElementType.TYPE })
 @Retention(RetentionPolicy.RUNTIME)
-//文档  所有的注解文档应该被 javadoc 记录的
 @Documented
 @RestController
 @RequestMapping
 public @interface RestApiController {
-    String name() default "";
-    
-    @AliasFor("path")
-    String[] value() default {"/api"};
-    
-    @AliasFor("value")
-    String[] path() default {"/api"};
-    
-    RequestMethod[] method() default {};
-    
-    String[] params() default {};
-    
-    String[] headers() default {};
-    
-    String[] consumes() default {};
-    
-    String[] produces() default {};
+    String name()                      default "";
+    @AliasFor("path") String[] value() default { "/api" };
+    @AliasFor("value") String[] path() default { "/api" };
+    RequestMethod[] method()           default {};
+    String[] params()                  default {};
+    String[] headers()                 default {};
+    String[] consumes()                default {};
+    String[] produces()                default {};
 }
+
