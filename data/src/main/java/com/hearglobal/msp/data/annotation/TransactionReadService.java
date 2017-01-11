f8839a32a48d81f5fd960a4bfccdc9c17be10178
ccg <chencg@hearglobal.com>
2017-01-11 15:35:25 +0800

    TransactionReadService and TransactionWriteService Annoactions create

---
+++ data/src/main/java/com/hearglobal/msp/data/annotation/TransactionReadService.java
@@ -0,0 +1,46 @@
+package com.hearglobal.msp.data.annotation;
+
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by Administrator on 2017/1/11.
+ *
+ * 封装带事务的 Service 的只读注解 默认超时5s
+ *
+ */
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Transactional
+@Service
+public @interface TransactionReadService {
+    @AliasFor("transactionManager")
+    String value() default "";
+
+    @AliasFor("value")
+    String transactionManager() default "";
+
+    Propagation propagation() default Propagation.REQUIRED;
+
+    Isolation isolation() default Isolation.DEFAULT;
+
+    int timeout() default 5;
+
+    boolean readOnly() default true;
+
+    Class<? extends Throwable>[] rollbackFor() default {};
+
+    String[] rollbackForClassName() default {};
+
+    Class<? extends Throwable>[] noRollbackFor() default {};
+
+    String[] noRollbackForClassName() default {};
+}
