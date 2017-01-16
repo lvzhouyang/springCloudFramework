8196f3d9f5260aa61f081ed629d1cb28cb331ac2
lvzy <lvzy@hearglobal.com>
2017-01-16 14:30:05 +0800

    event

--- core/src/main/java/com/hearglobal/msp/core/config/AppAsyncConfigurer.java
+++ core/src/main/java/com/hearglobal/msp/core/config/AppAsyncConfigurer.java
@@ -2,6 +2,7 @@ package com.hearglobal.msp.core.config;
 
 import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 import org.springframework.aop.interceptor.SimpleAsyncUncaughtExceptionHandler;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
@@ -10,6 +11,7 @@ import java.util.concurrent.Executor;
 /**
  * Created by lvzhouyang on 17/1/16.
  */
+@Configuration
 public class AppAsyncConfigurer implements AsyncConfigurer {
 
     @Override
