748fa2619f9a8fb556b4c6fde8153f4dd38a6f54
lvzy <lvzy@hearglobal.com>
2017-01-16 14:27:50 +0800

    event

---
+++ core/src/main/java/com/hearglobal/msp/core/config/AppAsyncConfigurer.java
@@ -0,0 +1,29 @@
+package com.hearglobal.msp.core.config;
+
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.aop.interceptor.SimpleAsyncUncaughtExceptionHandler;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+/**
+ * Created by lvzhouyang on 17/1/16.
+ */
+public class AppAsyncConfigurer implements AsyncConfigurer {
+
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(4);
+        executor.setMaxPoolSize(20);
+        executor.setThreadNamePrefix("MyExecutor-");
+        executor.initialize();
+        return executor;
+    }
+
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return new SimpleAsyncUncaughtExceptionHandler();
+    }
+}
