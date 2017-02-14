15832ba592df6fd86bd1a20bdda698fbea0100f5
lvzy <lvzy@hearglobal.com>
2017-02-14 14:53:53 +0800

    ribion

--- core/src/main/java/com/hearglobal/msp/core/remote/HttpRemoteService.java
+++ core/src/main/java/com/hearglobal/msp/core/remote/HttpRemoteService.java
@@ -9,6 +9,12 @@ import com.hearglobal.msp.util.ObjectUtil;
 import com.hearglobal.msp.util.StringUtil;
 import org.apache.commons.collections.MapUtils;
 import org.apache.commons.lang.math.NumberUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Map;
 
@@ -19,13 +25,22 @@ import java.util.Map;
  */
 public class HttpRemoteService {
 
+    protected Logger logger = LoggerFactory.getLogger(HttpRemoteService.class);
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     public static String DATA = "data";
     public static String STATUS = "status";
     public static String ERROR = "error";
 
     public <T> T invoke(Map<String, Object> response, TypeReference<T> typeReference) {
-
         if (MapUtils.isEmpty(response)) {
             throw new AppBusinessException("请求失败!");
         }
@@ -47,6 +62,36 @@ public class HttpRemoteService {
             throw new RemoteCallException(getDefaultError(), NumberUtils.toInt(getDefaultError().getCode()));
         }
     }
+
+    /**
+     * get template
+     *
+     * @param url
+     * @param uriVariables
+     * @param typeReference
+     * @param <T>
+     * @return
+     */
+    public <T> T invokeGet(String url, Map<String, Object> uriVariables, TypeReference<T> typeReference) {
+        // check
+        this.invokeGetCheck(url);
+        Map<String, Object> result;
+        try {
+            result = this.restTemplate.getForObject(url, Map.class, uriVariables);
+        } catch (Exception e) {
+            logger.error("HttpRemoteService invokeGet error url:{},uriVariables:{},typeReference:{}", url, ObjectUtil.toString(uriVariables), ObjectUtil.toString(typeReference));
+            throw new RemoteCallException(getDefaultError(), NumberUtils.toInt(getDefaultError().getCode()));
+        }
+        return this.invoke(result, typeReference);
+    }
+
+    private void invokeGetCheck(String url) {
+        if (StringUtil.isEmpty(url)) {
+            throw new RemoteCallException(new Error(CommonErrorCode.BAD_REQUEST.getCode(), "", "请求的URL为空!")
+                    , NumberUtils.toInt(CommonErrorCode.BAD_REQUEST.getCode()));
+        }
+    }
+
 
     private Error getDefaultError() {
         return new Error(CommonErrorCode.INTERNAL_ERROR.getCode(), "", "发生未知错误!");
